ee80bea991ecb06f5a904d540808b63090fef754
parkminha <mhtm_love@naver.com>
2022-09-29 16:00:31 +0900

    0929

--- front/src/views/Alltest.vue
+++ front/src/views/Alltest.vue
@@ -1,5 +1,49 @@
 <template>
-    <div>
-       
+    <v-layout class="background" align-center justify-center>
+    <v-layout class="inside" fill-height align-center justify-center>
+        <div>
+        <v-text-field label="키워드 검색"
+        placeholder="원하는 테스트의 키워드를 검색해보세요!"
+        v-model="keyword"
+        ></v-text-field>
+        {{ keyword }}
     </div>
-</template>
+        <div>
+      <v-btn @click="testStart">테스트 시작하기</v-btn>
+    </div>
+    </v-layout>
+    </v-layout>
+    </template>
+    
+    <script>
+      import HelloWorld from '../components/HelloWorld'
+    
+      export default {
+        name: 'Home',
+        methods: {
+          testStart(){
+            this.$router.push("/alltest");
+          }
+        },
+    
+        components: {
+          HelloWorld,
+        },
+      }
+    </script>
+    <style scoped>
+      .background{
+        height: 100px;
+        background-color: #F0F8FF;
+        height: 100%;
+            background-color: #E6E6FA;
+      }
+      .inside{
+        background-color: white;
+        width:1500px;
+        height: 750px;
+        margin-top: 50px;
+        border-radius: 100px 100px 100px 100px;
+      }
+    </style>
+    
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <template>
-  <v-layout class="background" align-center justify-center>
-  <div>
+<v-layout class="background" align-center justify-center>
+<v-layout class="inside" fill-height align-center justify-center>
     <v-text-field label="키워드 검색"
     placeholder="원하는 테스트의 키워드를 검색해보세요!"
     v-model="keyword"
     ></v-text-field>
     {{ keyword }}
+    
   <v-btn @click="testStart">테스트 시작하기</v-btn>
-</div>
+</v-layout>
 </v-layout>
 </template>
 
@@ -27,3 +28,18 @@
     },
   }
 </script>
+<style scoped>
+  .background{
+    height: 100px;
+    background-color: #F0F8FF;
+    height: 100%;
+        background-color: #E6E6FA;
+  }
+  .inside{
+    background-color: white;
+    width:1500px;
+    height: 750px;
+    margin-top: 50px;
+    border-radius: 100px 100px 100px 100px;
+  }
+</style>
