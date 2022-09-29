a1d0ac37055abb414e2f46f141db308cbcc61443
kimjina <cksehtk@naver.com>
2022-09-29 15:49:06 +0900

    dd

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,31 +1,32 @@
 <template>
-      <div class="background">
-        <v-layout fill-height align-center justify-center>
-          <div class="inside">
+  <div class="background">
+    <v-layout fill-height align-center justify-center>
+      <div class="inside">
 
-           <!--<v-btn>테스트 시작하기</v-btn>-->
-         </div>
-        </v-layout>
-        
-      </div>
+       <!--<v-btn>테스트 시작하기</v-btn>-->
+     </div>
+    </v-layout>
+    
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: "HomeView",
+name: "HomeView",
 };
 </script>
 <style scoped>
-  .background{
+.background{
+
+    background-color: #E6E6FA;
     height: 100%;
-        background-color: #E6E6FA;
-  }
-  .inside{
-    background-color: white;
-    width:1500px;
-    height: 750px;
-    margin-top: 50px;
-    border-radius: 100px 100px 100px 100px;
-  }
+}
+.inside{
+background-color: white;
+width:1500px;
+height: 750px;
+margin-top: 50px;
+border-radius: 100px 100px 100px 100px;
+}
 </style>
