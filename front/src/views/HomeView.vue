af47a41b3247555d1850d90bc1ce0961b09a5e35
cksehtk <cksehtk@naver.com>
2022-10-02 01:26:02 +0900

    폰트 등등 메인 이미지랑 이것저것~

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -3,16 +3,29 @@
   <v-layout class="background" align-center justify-center>
     <div>
   <v-layout class="inside" fill-height align-center justify-center>
-    <div class="text">
+    <div class="text" style="text-align : center;">
       <v-text-field label="키워드 검색"
       placeholder="원하는 테스트의 키워드를 검색해보세요!"
       v-model="keyword"
       ></v-text-field>
+      <div class="font" style="text-align : left;" >
+      └요즘 유행하는 테스트!
+      </div>
       {{ keyword }}
-      <div  style="display: flex; justify-content: center;">
+        <div class="image1">
+          <img src="../..\src\assets\dd.png">
+        </div>
+        <div class="image2">
+          <img src="../..\src\assets\dd.png">
+        </div>
+        <div class="image3">
+          <img src="../..\src\assets\dd.png">
+        </div>
+          <div class="image4">
+          <img src="../..\src\assets\dd.png">
+        </div>
     <v-btn @click="testStart">테스트 시작하기</v-btn>
       </div>
-  </div>
   </v-layout>
     </div>
   </v-layout>
@@ -49,9 +62,10 @@
     .text{
       width: 700px;
     }
-    .v-btn{
-      margin-top: 500px;
+    .image1, .image2, .image3, .image4{
+      display: inline-block;
+      padding: 20px;
+      margin-top:50px;
     }
-  
   </style>
   
