618a26066d3aa660ae279055fccf54317512c248
BrotherZin <ahj13k@naver.com>
2022-10-06 15:47:07 +0900

    싸이코 테스트 추가.

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -6,6 +6,7 @@ import Example1 from '../views/Example1.vue'
 import ExampleEnd from '../views/ExampleEnd.vue'
 import Foodtest from '../views/Foodtest.vue'
 import Foodtestgo from '../test/Foodtestgo.vue'
+import PsychoTest from '../views/PsychoTest.vue'
 
 Vue.use(VueRouter)
 
@@ -17,10 +18,7 @@ const routes = [
   },
 
   {
-
     path: '/Alltest',
-
-
     name: 'testStart',
     component: Alltest
   },
@@ -48,6 +46,11 @@ const routes = [
     path: '/Foodtestgo',
     name: 'foodtestgo',
     component: Foodtestgo
+  },
+  {
+    path: '/psychoTest',
+    name: 'psychotest',
+    component: PsychoTest
   },
 
 
