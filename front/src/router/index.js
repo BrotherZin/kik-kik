c0bbf322759d5fe76aafd6fa5482650262513d3f
BrotherZin <ahj13k@naver.com>
2022-10-27 17:51:57 +0900

    dk

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,32 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Alltest from '../views/Alltest.vue'
-
 import idealtypeStart from '../views/test/idealtype/idealtypeStart.vue'
 import idealtype1 from '../views/test/idealtype/idealtype1.vue'
 import idealtypeEnd from '../views/test/idealtype/idealtypeEnd.vue'
 import Foodtest from '../views/test/food/Foodtest.vue'
 import Foodtestgo from '../views/test/food/Foodtestgo.vue'
 import PsychoTest from '../views/test/psycho/PsychoTest.vue'
-import test from '../views/test.vue'
 import PsychoTest1 from '../views/test/psycho/PsychoTest1.vue'
 import PsychoTest2 from '../views/test/psycho/PsychoTest2.vue'
 import PsychoTest3 from '../views/test/psycho/PsychoTest3.vue'
 import PsychoTest4 from '../views/test/psycho/PsychoTest4.vue'
 import PsychoTest5 from '../views/test/psycho/PsychoTest5.vue'
-
-import idealtypeStart from '../test/idealtype/idealtypeStart.vue'
-import idealtype1 from '../test/idealtype/idealtype1.vue'
-import idealtypeEnd from '../test/idealtype/idealtypeEnd.vue'
-import Foodtest from '../test/food/Foodtest.vue'
-import Foodtestgo from '../test/food/Foodtestgo.vue'
-import PsychoTest from '../test/psycho/PsychoTest.vue'
-import idealtest from '../test/idealtype/idealtest.vue'
-import idealtest2 from '../test/idealtype/idealtest2.vue'
-import PsychoTest1 from '../test/psycho/PsychoTest1.vue'
-
-
-
 import Lolchang from '../views/test/lol/Lolchang.vue'
 import Lolchanggo2 from '../views/test/lol/Lolchanggo2'
 
