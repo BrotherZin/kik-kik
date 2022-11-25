3f6e019be51d5749f9862e2f1bcf57dd3fb53b55
cksehtk <cksehtk@naver.com>
2022-11-25 22:24:33 +0900

    대충 로고

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -2,7 +2,11 @@
     <div class="header">
         <v-layout>
             <div class="kik">
-            <a href="/"><img src="/image/logo.png"><span class="logo" >KIK KIK</span></a>
+            <a href="/"><img src="/image/logo.png">
+                
+                    <span class="logo" >KIK KIK</span>
+                
+            </a>
         </div>
             <v-spacer></v-spacer>
         </v-layout>
@@ -22,11 +26,16 @@ export default{
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Yeon+Sung&display=swap');
+*{
+    font-family: 'Yeon Sung', cursive;
+}
     .header{
         position: sticky;
         top: 0px; 
         background-color: white;
         z-index: 11;
+        box-shadow: 0 5px 5px 0 grey;
     }
     .header .logo{
         font-size:60px;
@@ -52,6 +61,7 @@ img{
     text-align: center;
   font-size: 70px;
   margin: 0;
+  display: flex;
 }
 img:hover {
 				transform: rotate( 45deg );
@@ -63,5 +73,48 @@ img:hover {
     -webkit-text-fill-color: transparent;
     font-size:50px;
     margin-left:10px;
+}
+span:hover{
+  position: relative;
+  top: 20px;
+  display: inline-block;
+  animation: bounce .3s ease infinite alternate;
+  font-family: 'Titan One', cursive;
+  font-size: 80px;
+  color: #FFF;
+  text-shadow: 0 1px 0 #CCC,
+               0 2px 0 #CCC,
+               0 3px 0 #CCC,
+               0 4px 0 #CCC,
+               0 5px 0 #CCC,
+               0 6px 0 transparent,
+               0 7px 0 transparent,
+               0 8px 0 transparent,
+               0 9px 0 transparent,
+               0 10px 10px rgba(#db469f 0%, #2188ff 100%);
+}
+
+span:nth-child(2) { animation-delay: .1s; }
+span:nth-child(3) { animation-delay: .2s; }
+span:nth-child(4) { animation-delay: .3s; }
+span:nth-child(5) { animation-delay: .4s; }
+span:nth-child(6) { animation-delay: .5s; }
+span:nth-child(7) { animation-delay: .6s; }
+span:nth-child(8) { animation-delay: .7s; }
+
+@keyframes bounce {
+  100% {
+    top: -20px;
+    text-shadow: 0 1px 0 #CCC, 
+                 0 2px 0 #CCC,
+                 0 3px 0 #CCC,
+                 0 4px 0 #CCC,
+                 0 5px 0 #CCC,
+                 0 6px 0 #CCC,
+                 0 7px 0 #CCC,
+                 0 8px 0 #CCC,
+                 0 9px 0 #CCC,
+                 0 50px 25px rgba(#db469f 0%, #2188ff 100%);
+  }
 }
 </style>
