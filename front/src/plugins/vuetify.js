f7211a5ba8f46846fee2afa704bc830dbf821055
cksehtk <cksehtk@naver.com>
2022-12-01 17:24:34 +0900

    별거없는 중간결산

--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -1,15 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 import VueTyperPlugin from 'vue-typer'
-import VueSocialSharing from 'vue-social-sharing'
-
-Vue.use(VueSocialSharing);
 
 Vue.use(VueTyperPlugin)
 
 Vue.use(Vuetify);
-
-
 
 export default new Vuetify({
     global: {
@@ -18,7 +13,4 @@ export default new Vuetify({
       icons: {
         iconfont: 'mdi',
       },
-      modules: [
-        'vue-social-sharing/nuxt'
-      ]
 });
